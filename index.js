a2798f1f468fa1ff665e5cb8f419eafae7470a5a
时勇勇 <961245677@qq.com>
2018-08-21 17:59:22 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+import OriginDecimal from 'decimal.js-light';
+
+const format = (num, defaultNumber = 0) =>
+  Number(Number.isFinite(Number(num)) ? num : defaultNumber) || defaultNumber;
+class Decimal extends OriginDecimal {
+  constructor(num) {
+    const number = format(num);
+    super(number);
+    if (num instanceof Decimal) {
+      return num;
+    }
+    this.constructor = Decimal;
+  }
+  div(num) {
+    return super.div.call(this, format(num, 1));
+  }
+
+  add(num) {
+    const number = format(num);
+    if (number) {
+      return super.add.call(this, number);
+    }
+    return this;
+  }
+  mul(num) {
+    return super.mul.call(this, format(num, 1));
+  }
+  minus(num) {
+    const number = format(num);
+    if (number) {
+      return super.minus.call(this, number);
+    }
+    return this;
+  }
+}
+
+
+export default function exDecimal(num) {
+  return new Decimal(num);
+}
+
+window.exDecimal = exDecimal;
